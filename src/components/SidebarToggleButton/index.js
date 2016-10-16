3c8c3ebfde7756584a6e3088c0d5616df311ed04
Joshua Comeau <joshwcomeau@gmail.com>
2016-10-16 13:54:07 -0400

    Small style tweaks to SidebarToggleButton

--- src/components/SidebarToggleButton/index.js
+++ src/components/SidebarToggleButton/index.js
@@ -11,7 +11,6 @@ const styles = StyleSheet.create({
     height: '30px',
     background: grey900,
     display: 'flex',
-    justifyContent: 'center',
     border: 0,
     padding: 0,
     borderRadius: '30px 0 0 30px',
@@ -31,10 +30,13 @@ const SidebarToggleButton = ({ mergeStyles, isVisible, toggleSidebar }) => {
         color={grey100}
         size={22}
         style={{
+          display: 'block',
           transform: isVisible ? 'rotateY(180deg)' : 'rotateY(0deg)',
           transition: 'transform 500ms',
           transitionDelay: '300ms',
           transformOrigin: 'center center',
+          marginTop: '2px',
+          marginLeft: '3px',
         }}
       />
     </button>
