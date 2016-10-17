e96ade7b1a5535205c63784b28485dff85e2acc1
Joshua Comeau <joshwcomeau@gmail.com>
2016-10-17 08:19:33 -0400

    Add deploy script and instructions

---
+++ DEPLOY.md
@@ -0,0 +1,14 @@
+## Deployment
+
+This project is deployed to GitHub Pages. The current deploy process is a bit funky.
+
+The `gh-pages` branch holds the production code, BUT we don't push the entirety of the contents to GitHub. This is because, AFAIK, there is no way to specify that GitHub Pages should read from a subdirectory, and the compiled application lives in /build.
+
+#### Instructions
+
+- checkout `gh-pages`. If on a new machine, create the local branch, don't try and pull the remote.
+- rebase master, so that it's up to date.
+- Remove `build` from .gitignore
+- run `npm run build`.
+- run `git add . && git commit --amend --no-edit`
+- run `npm run deploy`.
---
+++ tools/deploy.js
@@ -0,0 +1,34 @@
+// NOTE: This script handles the deployment, but NOT getting the Git branch
+// up-to-date. You need to rebase master before deploying.
+const git = require('git-rev');
+const { exec } = require('child_process');
+
+
+const deployBranch = 'gh-pages';
+
+function deploy(currentBranch) {
+  // Ensure that we're on the gh-pages branch
+  if (currentBranch !== deployBranch) {
+    console.error(`
+      DEPLOY FAILED.
+      You are currently on the '${currentBranch}' branch, and you need to be on '${deployBranch}'.
+
+      NEXT STEPS:
+      $ git checkout ${deployBranch}
+      $ git rebase master
+      $ npm run deploy
+    `);
+  }
+
+  // Split the current repo into a new branch consisting only of the
+  // `build` directory, and immediately force-push it to gh-pages branch.
+  exec(`git push origin \`git subtree split --prefix build ${deployBranch}\`:${deployBranch} -f`);
+
+  console.info(`
+    Successfully deployed!
+    Check it out: http://joshwcomeau.github.io/foodshow
+  `);
+}
+
+// `git.branch` takes a callback which is applied w/ the current branch name.
+git.branch(deploy);
